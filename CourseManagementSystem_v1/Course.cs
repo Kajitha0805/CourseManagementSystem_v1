cc90e134009132cfcac3a156f327916438ceae00
ut01222 <ut01222@UNICOMTIC10>
2024-10-11 14:09:54 +0530

    Add project files.

---
+++ CourseManagementSystem_v1/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Course
+    {
+         private string CourseId { get; set; }
+         private string Title {  get; set; }
+         private string Duration { get; set; }
+         private decimal Price {  get; set; }
+
+
+        public Course(string courseId,string title,string duration,decimal price)
+        {
+            CourseId = courseId;
+            Title=title;
+            Duration=duration;
+            Price=price;
+        }
+        public override string ToString()
+        {
+            return $"CourseId:{CourseId},Title:{Title},Duration:{Duration},Price:{Price}";
+        }
+    }
+}
---
+++ CourseManagementSystem_v1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course1 = new Course("C_001", "python for beginners", "3 months", 10);
+            Console.WriteLine(course1.ToString());
+            Console.ReadLine();
+        }
+    }
+}
